9d25be58da893e9c28fd2a48797027dc81437c60
andreysc@yandex.ru <andreysc@yandex.ru>
2020-01-19 19:03:42 +0300

    Объект для работы с облаком

---
+++ onedrive.py
@@ -0,0 +1,11 @@
+import os
+
+class onedrive(object):
+    def __init__(self,token):
+        self.token = token
+
+    def uploadfile(self,filename):
+        a=1
+
+    def downloadfile(self,filename,dfilename):
+        a=1

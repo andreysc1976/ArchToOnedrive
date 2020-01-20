f4a978cf39522caa182a2602a10180a58b5357b8
andreysc@yandex.ru <andreysc@yandex.ru>
2020-01-20 23:15:15 +0300

    2

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ for dir in DIRS:
     for root,path,files in os.walk(dir):
         for file in files:
             fullname = os.path.join(root,file)
-            #adb.inserttodb(fullname)
+            adb.inserttodb(fullname)
 adb.listbase()
